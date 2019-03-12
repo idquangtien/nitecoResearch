aaacf2e059051f92053b535356b63b648ba89d74
tien.dang <tien.dang@niteco.se>
2019-03-12 17:48:00 +0700

    >>>>>>>>>>>>prepros build<<<<<<<<<

---
+++ nitecoResearch/research/data/result.php
@@ -0,0 +1,12 @@
+// Nhập giá trị number bằng phương thức post
+$number = isset($_POST['number']) ? (int)$_POST['number'] : false;
+ 
+// Kiểm tra number có lớn hơn không hay không
+if (!$number){
+    die ('<h1>Vui lòng nhập một số lớn hơn không (0)</h1>');
+}
+ 
+// Lặp từ 1 tới number để in ra màn hình
+for ($i = 1; $i <= $number; $i++){
+    echo '<li>Số: '.$i.'</li>';
+}
