494338d918d0bf7986f7621146f51ab1027443bb
tien.dang <tien.dang@niteco.se>
2019-03-15 17:25:06 +0700

    add product list

--- nitecoResearch/research/assets/js/main.js
+++ nitecoResearch/research/assets/js/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-
-    
-    
-
-});
-
 class ComponentSlideshow {
     constructor(props){
        this.container = props.container;
@@ -83,6 +76,96 @@ class ComponentSlideshow {
         let refreshIntervalId = setInterval(interval, this.timePause);
     }
 }
+
+class ComponentCarousel {
+    constructor(props){
+        this.container = props.container;
+        this.buttonNext = props.buttonNext;
+        this.buttonPrev = props.buttonPrev;
+        this.itemShow = props.itemShow ? props.itemShow : 5;
+        this.space = props.space ? props.space : '20';
+    }
+    init(){
+        let self = this;
+        let windowWidth = this.container.parent().width();
+        
+        let totalItem = this.container.find('li').length;
+
+        
+
+        let itemWidth = windowWidth / this.itemShow;
+        
+        
+
+        let container = itemWidth * totalItem;
+
+
+
+        this.container.width(container);
+        this.container.find('li').css('width', itemWidth + 'px');
+
+        
+        
+
+        this.buttonNext.click(function(){
+            let first = self.container.find('li').first();
+            first.animate({
+                'width': 0,
+            }, function(){
+                first.appendTo(self.container).css({
+                    'width': itemWidth
+                })
+            });
+        });
+        this.buttonPrev.click(function(){
+            let last = self.container.find('li').last().css({
+                'width': 0,
+            });
+            last.prependTo(self.container);
+            last.animate({'width': itemWidth});
+        });
+    }
+}
+
+class ComponentGridPinteres {
+    constructor(props){
+       this.grid = props.grid;
+    }
+    resizeGridItem(item) {
+        let rowHeight = parseInt(this.grid.css('grid-auto-rows'));
+
+        let rowGap = parseInt(this.grid.css('grid-row-gap'));
+       
+        let rowSpan = Math.ceil( (item.find(".gridItem__content").outerHeight() + rowGap) / (rowHeight + rowGap) );
+
+        $(item).css("grid-row-end","span " + rowSpan);
+    }
+    resizeAllGridItems(){
+        let items = $(".gridItem");
+        items.each((index, item)=>{
+            this.resizeGridItem($(item));
+        })
+    }
+    fullImageGridItems(){
+        let items = $(".gridItem");
+        items.each((index, item)=>{
+            $(item).find("img").addClass("full");
+        })
+    }
+    init(){
+        let self = this;
+        let make_promise = function(task = function(){}){
+            return new Promise(function(resolve, reject){
+                task();
+                resolve();
+            });
+        }
+        make_promise(self.resizeAllGridItems())
+            .then(function(){
+                self.fullImageGridItems();
+            });
+    }
+}
 $(document).ready(function(){
     let slideshow = new ComponentSlideshow({
         container: $("#slideshow .slideshow__container"),
@@ -92,11 +175,24 @@ $(document).ready(function(){
 
     slideshow.init();
 
+    let carousel = new ComponentCarousel({
+        container: $('.carousel .carousel__container'),
+        buttonNext: $('.btn-next'),
+        buttonPrev: $('.btn-prev')
+    });
+    carousel.init();
+
     $(".collection > li").click(function(){
         $(this).children('ul').slideToggle();
     });
-});
-$(window).on("load",function(){
+
+
     
+
 });
+
+
+
+
+
 
